66da634e706ff9ac45cc702685a820f58807def7
H4-Des <oliveira.gonzales@gmail.com>
2023-01-08 23:44:49 -0300

    Carrega dados do localStorage ao carregar index.html

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
-const itens = []; //array para armazenar os itens adicionados como objetos
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //array para armazenar os itens adicionados como objetos
+
+itens.forEach((elemento) => {
+    adicionaElemento(elemento);
+});
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const nome = e.target.elements['nome']; //variável nome aponta para o input 'nome'
     const quantidade =  e.target.elements['quantidade'];    //variável quantidade aponta para o input 'quantidade'
-    
-       
-    adicionaElemento(nome.value,quantidade.value);
-    nome.value = "";
-    quantidade.value= "";
-});
-
-function adicionaElemento(nome, quantidade){
-    const novoItem = document.createElement("li");
-    const qtdNovoItem = document.createElement("strong");
-
-    //objeto para armazenar o item/quantidade e enviá-lo para localStorage
+     //objeto para armazenar o item/quantidade e enviá-lo para localStorage
     const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
+        "nome": nome.value,
+        "quantidade": quantidade.value
     };
 
-    itens.push(itemAtual); //insere um objeto novo na array itens
+    itens.push(itemAtual); //insere um objeto novo na array itens   
     localStorage.setItem("itens", JSON.stringify(itens)); //converte a array itens para string do tipo JSON
 
-    //cria novo item na lista do HTML
-    novoItem.classList.add("item");
-    qtdNovoItem.innerHTML = quantidade;
+    adicionaElemento(itemAtual);
+    nome.value = "";
+    quantidade.value= "";
+    nome.focus();
+});
+
+function adicionaElemento(item){
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item"); //cria novo item na lista do HTML
+
+    const qtdNovoItem = document.createElement("strong");
+    qtdNovoItem.innerHTML = item.quantidade;
     novoItem.appendChild(qtdNovoItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
     lista.appendChild(novoItem);
     
 }
