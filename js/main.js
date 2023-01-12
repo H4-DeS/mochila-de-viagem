f5ee06a08060373e8544bb6882c3d661ca9da680
H4-Des <oliveira.gonzales@gmail.com>
2023-01-11 22:15:06 -0300

    Verifica se um elemento novo já existe na lista e atualiza sua quantidade

--- js/main.js
+++ js/main.js
@@ -10,16 +10,27 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const nome = e.target.elements['nome']; //variável nome aponta para o input 'nome'
     const quantidade =  e.target.elements['quantidade'];    //variável quantidade aponta para o input 'quantidade'
-     //objeto para armazenar o item/quantidade e enviá-lo para localStorage
+     
+    //objeto para armazenar o item/quantidade e enviá-lo para localStorage
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     };
 
-    itens.push(itemAtual); //insere um objeto novo na array itens   
-    localStorage.setItem("itens", JSON.stringify(itens)); //converte a array itens para string do tipo JSON
+    let existe = itens.find(e => e.nome === nome.value);
+        
+    if(existe){
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+    } else {
+        itemAtual.id = itens.length;
+        
+        adicionaElemento(itemAtual);
+        itens.push(itemAtual); //insere um objeto novo na array itens   
+              
+    }
 
-    adicionaElemento(itemAtual);
+    localStorage.setItem("itens", JSON.stringify(itens)); //converte a array itens para string do tipo JSON 
     nome.value = "";
     quantidade.value= "";
     nome.focus();
@@ -28,11 +39,18 @@ form.addEventListener("submit", (e) => {
 function adicionaElemento(item){
     const novoItem = document.createElement("li");
     novoItem.classList.add("item"); //cria novo item na lista do HTML
-
     const qtdNovoItem = document.createElement("strong");
+    qtdNovoItem.dataset.id = item.id;
     qtdNovoItem.innerHTML = item.quantidade;
     novoItem.appendChild(qtdNovoItem);
     novoItem.innerHTML += item.nome;
     lista.appendChild(novoItem);
     
+}
+
+function atualizaElemento(item){
+    
+    document.querySelector('[data-id="' + item.id + '"]').innerHTML = item.quantidade;
+    itens[item.id].quantidade = item.quantidade; 
+
 }
